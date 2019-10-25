679172027bbf2f354eb64a229acf71f56c727dea
andrewtrieu <trieua2000@yahoo.com>
2019-10-24 18:17:13 -0700

    Create BreweryClient and test them.

---
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,72 @@
+package guru.springframework.msscbreweryclient.web.client;
+
+import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import guru.springframework.msscbreweryclient.web.model.CustomerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient client;
+
+    @Test
+    void getBeerById() {
+        BeerDto dto = client.getBeerById(UUID.randomUUID());
+        assertNotNull(dto);
+    }
+
+    @Test
+    void testSaveNewBeer() {
+        //given
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+        URI uri = client.saveNewBeer(beerDto);
+        assertNotNull(uri);
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void testUpdateBeer() {
+        // given
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+        client.updateBeer(UUID.randomUUID(), beerDto);
+    }
+
+    @Test
+    void testDeleteBeer() {
+        client.deleteBeer(UUID.randomUUID());
+    }
+
+    @Test
+    void getCustomerById() {
+        CustomerDto customerDto = client.getCustomerById(UUID.randomUUID());
+        assertNotNull(customerDto);
+    }
+
+    @Test
+    void testSaveNewCustomer() {
+        CustomerDto customerDto = CustomerDto.builder().name("John").build();
+        URI uri = client.saveNewCustomer(customerDto);
+        assertNotNull(uri);
+        System.out.println(uri);
+    }
+
+    @Test
+    void testUpdateCustomer() {
+        CustomerDto customerDto = CustomerDto.builder().name("Doe").build();
+        client.updateCustomer(UUID.randomUUID(), customerDto);
+    }
+
+    @Test
+    void testDeleteCustomer() {
+        client.deleteCustomer(UUID.randomUUID());
+    }
+}
